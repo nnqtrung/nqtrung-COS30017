50af0beef454ede4a1153977caf95ab8bc117c19
nnqtrung <nqtrung123tr@gmail.com>
2026-09-25 16:53:05 +0700

    Updated week 2

---
+++ 30017-week 2/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var btnOK: Button;
+    lateinit var txtName: EditText;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnOK = findViewById(R.id.btnOK)
+        txtName = findViewById(R.id.txtName)
+
+        btnOK.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        var text = txtName.text
+        when(v?.id) {
+            R.id.btnOK->{txtName.setText("Hello $text")}
+        }
+    }
+}
